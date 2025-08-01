59a0e9c15e2e6ff337ff940f3edb4495e1c34e2a
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-08-01 09:56:13 +0530

    c# day 1

---
+++ 191/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+using System;
+namespace SetBit
+{
+    class SetBit
+    {
+        public static int FindBinary(int n)
+        {
+            string binary = "";
+            int count = 0;
+            while (n > 0)
+            {
+                int rem = n % 2;
+                binary = rem + binary;
+                n = n / 2;
+            }
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[i] == '1')
+                    count++;
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            SetBit.FindBinary(10);
+        }
+    }
+}
