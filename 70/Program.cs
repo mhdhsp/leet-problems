2e0f7b17dc70ea0a1145f677fc7a79eac7281658
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-08-06 16:26:50 +0530

    70

---
+++ 70/Program.cs
@@ -0,0 +1,35 @@
+﻿// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+using System;
+namespace SetBit
+{
+    class SetBit
+    {
+        public class Solution
+        {
+            public static int ClimbStairs(int n)
+            {
+                if (n == 0 || n == 1) return 1;
+                int first = 1, second = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    int temp = second;
+                    second = first + second;
+                    first = temp;
+                }
+                return second;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enterthe num of stairs");
+            int n = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(Solution.ClimbStairs(n));
+
+        }
+    }
+}
