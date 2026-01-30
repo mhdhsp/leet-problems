4a1e08966d0e95ffb44422ac0ae5d11f9a734636
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2026-01-30 11:01:12 +0530

    175

--- 191/obj/Debug/net9.0/191.AssemblyInfo.cs
+++ 191/obj/Debug/net9.0/191.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("191")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6f0975b0f0f47fad75178cedce7a3733a8f3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c8ae1be7d691a17ead39d60b844e50dc0516ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("191")]
 [assembly: System.Reflection.AssemblyTitleAttribute("191")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
